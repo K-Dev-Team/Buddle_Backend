075ecf4a8c8cbba2886d30ccf077bb2ce9791578
Son-Sumin <goldkitty007@gmail.com>
2023-06-03 15:58:20 +0900

    Buddle

---
+++ buddle/src/test/java/com/kdt/buddle/BuddleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kdt.buddle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuddleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
